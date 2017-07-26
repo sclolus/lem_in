e75b1033cca45ca2e8f4252392d29e5f268cbc98
Sebastien CLOLUS <sclolus@e3r9p9.42.fr>
2017-07-26 18:38:25 +0200

    dump, ajout du Makefile (wtf?)

---
+++ srcs/parsing/ft_check_integrity.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_integrity.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/26 13:48:13 by sclolus           #+#    #+#             */
+/*   Updated: 2017/07/26 13:50:12 by sclolus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+void	ft_check_integrity(t_lem_in_data *lem_in_data)
+{
+	if (!lem_in_data->room_nbr || !lem_in_data->lem_nbr
+		|| !ft_get_set_states()[0] || !ft_get_set_states()[1])
+	{
+		ft_putendl_fd(LEM_IN_ERR, 2);
+		exit(EXIT_FAILURE);
+	}
+}
