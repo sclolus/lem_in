10be385a482e6f7efc901ec0ed62adb890e63632
Sebastien CLOLUS <sclolus@e3r9p9.42.fr>
2017-07-26 18:49:17 +0200

    Norme de ft_parsing_actions.c

--- srcs/parsing/ft_parsing_actions.c
+++ srcs/parsing/ft_parsing_actions.c
@@ -6,16 +6,17 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/10 04:38:48 by sclolus           #+#    #+#             */
-/*   Updated: 2017/07/25 15:53:48 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/07/26 18:49:07 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int32_t			ft_error_case(char *line __attribute__((unused))
-			, t_lem_in_data *lem_in_data __attribute__((unused)))
+int32_t			ft_error_case(char *line
+							, t_lem_in_data *lem_in_data)
 {
+	(void)line;
+	(void)lem_in_data;
 	ft_putendl_fd(LEM_IN_ERR "\n", 2);
 	return (0);
 }
-
