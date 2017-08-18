c1fce49f7718a93777e1cd6e53014de0d6e63a2b
Sebastien CLOLUS <sclolus@e3r1p13.42.fr>
2017-08-18 07:13:08 +0200

    dump:

--- srcs/parsing/ft_check_integrity.c
+++ srcs/parsing/ft_check_integrity.c
@@ -6,7 +6,7 @@
 /*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/26 13:48:13 by sclolus           #+#    #+#             */
-/*   Updated: 2017/08/18 01:29:08 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/08/18 07:13:02 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_check_integrity(t_lem_in_data *lem_in_data)
 	if (!lem_in_data->room_nbr || !lem_in_data->lem_nbr
 		|| !ft_get_set_states()[0] || !ft_get_set_states()[1])
 	{
-		ft_putnbr((int)lem_in_data->lem_nbr);
 		ft_putendl_fd(LEM_IN_ERR, 2);
 		exit(EXIT_FAILURE);
 	}
